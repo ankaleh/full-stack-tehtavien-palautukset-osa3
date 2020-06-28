268007da33ac9f1a51f0059e6e6f7d15018e11f3
Anna Lehto <anviinamaki@gmail.com>
2020-06-28 14:14:54 +0300

    Osa 3: tehtävät 3.9. - 3.12.

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const userName = process.argv[2]
+const password = process.argv[3] //suoritus komennolla node mongo.js tietokantakayttaja tietokantakayttajan_salasana
+const url = `mongodb+srv://${userName}:${password}@cluster0-tyays.mongodb.net/puhelinnumerot?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const name = process.argv[4]
+const number = process.argv[5]
+
+const personSchema = new mongoose.Schema({ //luodaan skeema
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema) //määritetään model Person --> Mongoose luo taulun nimeltä People
+
+if (process.argv.length===6) {
+    const person = new Person({
+        name: name,
+        number: number
+    })
+    person.save().then(result => {
+        console.log('Numero tallennettu tietokantatauluun People.')
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(result => {
+        console.log(process.argv.length)
+        console.log('Puhelinluettelo: ')
+        
+        result.forEach(person => {
+            console.log(person)
+    
+        })
+        mongoose.connection.close()
+    })
+
+}
+
+
+
+
